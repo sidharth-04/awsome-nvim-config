6b4daf73debefdd2e1ec0857ce9038f11761f816
Sidharth Roy <68526404+sidharth-04@users.noreply.github.com>
2024-01-11 19:16:16 +0530

    Initial commit for my nvim config.

---
+++ init.lua
@@ -0,0 +1,132 @@
+-- LEADER
+-- These keybindings need to be defined before the first /
+-- is called; otherwise, it will default to "\"
+vim.g.mapleader = " "
+vim.g.localleader = "\\"
+
+vim.wo.relativenumber = true
+
+vim.api.nvim_set_option('shiftwidth', 4)
+vim.api.nvim_set_option('tabstop', 4)
+
+vim.opt.termguicolors = true
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  "folke/which-key.nvim",
+  { "folke/neoconf.nvim", cmd = "Neoconf" },
+  "folke/neodev.nvim",
+  {
+    "folke/tokyonight.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      -- load the colorscheme here
+      vim.cmd([[colorscheme tokyonight]])
+    end,
+  },
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.5',
+     dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  {
+    {"nvim-treesitter/nvim-treesitter",   ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false, build = ":TSUpdate"}},
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+    require("nvim-tree").setup {}
+    end,
+  },
+  {
+    "alexghergh/nvim-tmux-navigation",
+    event = "VeryLazy",
+    config = function()
+      local nvim_tmux_nav = require("nvim-tmux-navigation")
+      nvim_tmux_nav.setup({
+        keybindings = {
+          left = "<C-h>",
+          down = "<C-j>",
+          up = "<C-k>",
+          right = "<C-l>",
+        },
+      })
+    end,
+  },
+  {
+    'romgrk/barbar.nvim',
+    dependencies = {
+      'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+      'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+    },
+    init = function() vim.g.barbar_auto_setup = false end,
+    opts = {
+      sidebar_filetypes = {
+        NvimTree = true,
+      },
+    },
+    version = '^1.0.0', -- optional: only update when a new 1.x version is released
+  },
+  {
+    'stevearc/oil.nvim',
+    opts = {},
+    -- Optional dependencies
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+  {
+    'numToStr/Comment.nvim',
+    opts = {
+        -- add any options here
+    },
+    lazy = false,
+  }
+})
+
+-- Key Bindings
+-- Telescope
+local opts = { noremap = true, silent = true }
+local map = vim.api.nvim_set_keymap
+local vmap = vim.keymap.set
+vmap("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
+vmap("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
+vmap("n", "<leader>gp", "<cmd>Telescope live_grep<cr>")
+map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
+map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
+map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
+map('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
+map('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
+map('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
+map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
+map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
+map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
+
+-- NvimTree
+vmap("n", "<C-n>", "<cmd>NvimTreeToggle<cr>")
+vmap("n", "<leader>e", "<cmd>NvimTreeFocus<cr>")
+
+-- Barbar
+vmap("n", "<leader>,", "<cmd>BufferNext<cr>")
+vmap("n", "<leader>.", "<cmd>BufferPrevious<cr>")
+vmap("n", "<leader>c", "<cmd>BufferClose<cr>")
+
+-- Oil
+map("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
