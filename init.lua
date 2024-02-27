3b9c869f5efffb9528bcf12b36f88ac74d680911
Sidharth Roy <68526404+sidharth-04@users.noreply.github.com>
2024-02-27 13:02:36 -0500

    Update config

--- init.lua
+++ init.lua
@@ -1,111 +1,166 @@
--- LEADER
--- These keybindings need to be defined before the first /
--- is called; otherwise, it will default to "\"
 vim.g.mapleader = " "
 vim.g.localleader = "\\"
 
+vim.wo.number = true
 vim.wo.relativenumber = true
-
-vim.api.nvim_set_option('shiftwidth', 4)
-vim.api.nvim_set_option('tabstop', 4)
-
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.swapfile = false
+vim.opt.compatible = false
 vim.opt.termguicolors = true
+vim.opt.clipboard = "unnamedplus"
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  "folke/which-key.nvim",
-  { "folke/neoconf.nvim", cmd = "Neoconf" },
-  "folke/neodev.nvim",
-  {
-    "folke/tokyonight.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme tokyonight]])
-    end,
-  },
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
-     dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  {
-    {"nvim-treesitter/nvim-treesitter",   ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
+    { "folke/neoconf.nvim", cmd = "Neoconf" },
+    "folke/neodev.nvim",
+    {
+        "folke/tokyonight.nvim",
+        lazy = false, -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+        config = function()
+            -- load the colorscheme here
+            vim.cmd([[colorscheme tokyonight]])
+        end,
+    },
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.5',
+         dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    {
+        {"nvim-treesitter/nvim-treesitter",     ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown" },
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false, build = ":TSUpdate"}},
-  {
-    "nvim-tree/nvim-tree.lua",
-    version = "*",
-    lazy = false,
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    },
-    config = function()
-    require("nvim-tree").setup {}
-    end,
-  },
-  {
-    "alexghergh/nvim-tmux-navigation",
-    event = "VeryLazy",
-    config = function()
-      local nvim_tmux_nav = require("nvim-tmux-navigation")
-      nvim_tmux_nav.setup({
-        keybindings = {
-          left = "<C-h>",
-          down = "<C-j>",
-          up = "<C-k>",
-          right = "<C-l>",
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false, build = ":TSUpdate", lazy=true}},
+    {
+        "nvim-tree/nvim-tree.lua",
+        version = "*",
+        lazy = false,
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
         },
-      })
-    end,
-  },
-  {
-    'romgrk/barbar.nvim',
-    dependencies = {
-      'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
-      'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+        config = function()
+        require("nvim-tree").setup {}
+        end,
     },
-    init = function() vim.g.barbar_auto_setup = false end,
-    opts = {
-      sidebar_filetypes = {
-        NvimTree = true,
-      },
+    {
+        "alexghergh/nvim-tmux-navigation",
+        event = "VeryLazy",
+        config = function()
+            require'nvim-tmux-navigation'.setup {
+                disable_when_zoomed = true
+            }
+        end,
     },
-    version = '^1.0.0', -- optional: only update when a new 1.x version is released
-  },
-  {
-    'stevearc/oil.nvim',
-    opts = {},
-    -- Optional dependencies
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-  },
-  {
-    'numToStr/Comment.nvim',
-    opts = {
-        -- add any options here
+    {
+        'romgrk/barbar.nvim',
+        dependencies = {
+            'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+            'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+        },
+        init = function() vim.g.barbar_auto_setup = false end,
+        opts = {
+            sidebar_filetypes = {
+                NvimTree = true,
+            },
+        },
+        version = '^1.0.0', -- optional: only update when a new 1.x version is released
     },
-    lazy = false,
-  }
+    {
+        'stevearc/oil.nvim',
+        opts = {},
+        -- Optional dependencies
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+    },
+    {
+        'numToStr/Comment.nvim',
+        opts = {
+                -- add any options here
+        },
+        lazy = false,
+    },
+    {
+        'neovim/nvim-lspconfig'
+    },
+    {
+        'simrat39/rust-tools.nvim'
+    },
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = function() vim.fn["mkdp#util#install"]() end,
+    },
+    {
+        "lewis6991/gitsigns.nvim",
+        config=function()
+			require("gitsigns").setup()
+        end,
+    },
+    {
+        "tpope/vim-fugitive"
+    },
+	{
+	  'stevearc/conform.nvim',
+	  opts = {},
+	},
+	{
+		'dstein64/vim-startuptime'
+	}
+})
+
+-- Rust Setup
+local rt = require("rust-tools")
+rt.setup({
+    server = {
+        on_attach = function(_, bufnr)
+            -- Hover actions
+            vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+            -- Code action groups
+            vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+        end,
+    },
+})
+
+-- Conform Setup
+require("conform").setup({
+	notify_on_error=false,
+  	formatters_by_ft = {
+		lua = { "stylua" },
+		python = { "isort", "black" },
+		javascript = { { "prettierd", "prettier" } },
+  	},
+	format_on_save = {
+		timeout_ms = 500,
+		lsp_fallback = true,
+  	},
 })
 
 -- Key Bindings
--- Telescope
 local opts = { noremap = true, silent = true }
 local map = vim.api.nvim_set_keymap
 local vmap = vim.keymap.set
+
+-- Nvim Tmux Navigation
+vmap('n', "<C-h>", "<cmd>NvimTmuxNavigateLeft<cr>")
+vmap('n', "<C-j>", "<cmd>NvimTmuxNavigateDown<cr>")
+vmap('n', "<C-k>", "<cmd>NvimTmuxNavigateUp<cr>")
+vmap('n', "<C-l>", "<cmd>NvimTmuxNavigateRight<cr>")
+
+-- Telescope
 vmap("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
 vmap("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
 vmap("n", "<leader>gp", "<cmd>Telescope live_grep<cr>")
@@ -130,3 +185,15 @@ vmap("n", "<leader>c", "<cmd>BufferClose<cr>")
 
 -- Oil
 map("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+-- Mardown Preview
+vmap("n", "<leader>mp", "<cmd>MarkdownPreview<cr>")
+vmap("n", "<leader>mc", "<cmd>MarkdownPreviewStop<cr>")
+
+-- Neorg
+map('n', "<leader>nw", "'<cmd>Neorg workspace ' . input('Enter workspace: ') . '<cr>'", {expr = true, noremap = true})
+
+-- Gitsigns
+map('n', "[h", "<cmd>Gitsigns prev_hunk<cr>", opts)
+map('n', "]h", "<cmd>Gitsigns next_hunk<cr>", opts)
+map('n', "<leader>gs", "<cmd>Gitsigns preview_hunk_inline<cr>", opts)
